d6692a718e5459fda46d6a807045d97e94684c9c
Rakesh <rakeshkumar8900@gmail.com>
2022-01-25 16:47:40 +0530

    request limit increased

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,15 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import fileUpload from 'express-fileupload';
 import  masterRouter from './routes/index';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 const port = 5001;
+
+//set upload limit
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(fileUpload({
   useTempFiles : true,
